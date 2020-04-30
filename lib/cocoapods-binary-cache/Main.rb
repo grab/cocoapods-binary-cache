5221c7b95c710601bc54982491c3163209cd2bd9
Ngoc Thuyen Trinh <chris.thuyen@grabtaxi.com>
2020-04-30 02:50:35 +0000

    Delete Main.rb

--- lib/cocoapods-binary-cache/Main.rb
+++
@@ -1,8 +0,0 @@
-# Copyright 2019 Grabtaxi Holdings PTE LTE (GRAB), All rights reserved.
-# Use of this source code is governed by an MIT-style license that can be found in the LICENSE file
-
-# Provide DSL options to set from Podfile
-require_relative 'pod-binary/prebuild_dsl'
-
-# Hook cocoapods pre-install and post-install to do prebuild, caching stuffs
-require_relative 'pod-binary/prebuild_hook'
