a2dec0bdf46cc9f10e211bbf3c8792faeb63d409
Bang Nguyen <bang.nguyen@grab.com>
2020-05-27 16:36:25 +0800

    Close #5 Close #2

--- lib/command/PythonScripts/prebuild_lib.py
+++ lib/command/PythonScripts/prebuild_lib.py
@@ -25,7 +25,6 @@ class PrebuildLib:
         self.cache_repo = config.cache_repo
         self.cache_path = config.cache_path
         self.prebuild_path = config.prebuild_path
-        # self.generated_dir_name = config.generated_dir_name
         self.delta_path = config.delta_path
         self.manifest_file = config.manifest_file
         self.devpod_cache_repo = config.devpod_cache_repo
@@ -123,7 +122,7 @@ class PrebuildLib:
 
     def prebuild_if_needed(self, push=True, branch='master'):
         self.fetch_and_apply_cache(branch=branch)
-        self.pod_install_update_ifneeded()
+        self.pod_install_update_if_needed()
         # Sync with cache directory
 
         if not os.path.isfile(self.delta_path):
@@ -160,20 +159,21 @@ class PrebuildLib:
         except Exception as e:
             raise e
 
-    def prebuild_devpod(self):
+    # If you use other type of server for file caching such as S3 or FTP server =>
+    # use prebuild_devpod function and do the upload/download separately
+    def prebuild_devpod(self, push=False, try_repo_update=False):
         self.fetch_and_apply_cache()
         self.fetch_and_apply_devpod_cache()
-        self.pod_install_update_ifneeded()
+        self.pod_install_update_if_needed(try_repo_update)
+        if push:
+            ZipUtils.zip_subdirs(self.devpod_prebuild_output, self.devpod_cache_libs_path)
+            self.push_all_to_git(self.devpod_cache_libs_path)
 
-    # If you use other things for file caching such as S3 or FTP server =>
-    # use prebuild_devpod function and do the upload/download separately
-    def prebuild_devpod_and_push(self):
-        self.prebuild_devpod()
-        ZipUtils.zip_subdirs(self.devpod_prebuild_output, self.devpod_cache_libs_path)
-        self.push_all_to_git(self.devpod_cache_libs_path)
-
-    def pod_install_update_ifneeded(self):
-        try:
+    def pod_install_update_if_needed(self, try_repo_update=False):
+        if try_repo_update:
+            try:
+                subprocess.run(['bundle', 'exec', 'pod', 'install'], check=True)
+            except subprocess.CalledProcessError:
+                subprocess.run(['bundle', 'exec', 'pod', 'install', '--repo-update'], check=True)
+        else:
             subprocess.run(['bundle', 'exec', 'pod', 'install'], check=True)
-        except subprocess.CalledProcessError:
-            subprocess.run(['bundle', 'exec', 'pod', 'install', '--repo-update'], check=True)
