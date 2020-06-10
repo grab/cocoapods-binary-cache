20c7acb775fe182528a4fe3b17319e75ecc74ede
Chris Thuyen <chris.thuyen@grabtaxi.com>
2020-06-10 14:43:14 +0800

    chore(*): exclude dev pods when validating with podfile, when prebuilt dev pods are not enabled

--- lib/cocoapods-binary-cache/cache/validator_with_podfile.rb
+++ lib/cocoapods-binary-cache/cache/validator_with_podfile.rb
@@ -1,7 +1,15 @@
 module PodPrebuild
   class PodfileChangesCacheValidator < BaseCacheValidator
     def validate(*)
-      validate_with_podfile
+      return PodPrebuild::CacheValidationResult.new if @prebuilt_lockfile.nil? || @podfile.nil?
+
+      validation = validate_with_podfile
+      unless Pod::Podfile::DSL.dev_pods_enabled
+        dev_pods_in_podfile = @podfile.dependencies.select(&:local?).map(&:name)
+        dev_pods_in_prebuilt_lockfile = @prebuilt_lockfile.nil? ? [] : @prebuilt_lockfile.dev_pod_names.to_a
+        return validation.exclude_pods(dev_pods_in_podfile + dev_pods_in_prebuilt_lockfile)
+      end
+      validation
     end
   end
 end
---
+++ spec/cahe_validator/validator_with_podfile_spec.rb
@@ -0,0 +1,46 @@
+describe "PodPrebuild::PodfileChangesCacheValidator" do
+  describe "#validate" do
+    let(:pods) do
+      {
+        "A" => { :version => "0.0.5" },
+        "B" => { :version => "0.0.5" },
+        "C" => { :version => "0.0.5" }
+      }
+    end
+    let(:prebuilt_lockfile) { gen_lockfile(pods: pods) }
+    let(:dev_pods_enabled) { true }
+    let(:podfile) do
+      Pod::Podfile.new do
+        source "https://cdn.cocoapods.org/"
+        pod "A", "0.0.6"  # Updated
+        pod "B", "0.0.5"
+        pod "C", "0.0.5"
+        pod "D", "0.0.5"  # Added
+        pod "E", :path => "Local/" # Added, but local
+      end
+    end
+
+    before do
+      allow(Pod::Podfile::DSL).to receive(:dev_pods_enabled).and_return(dev_pods_enabled)
+      validation_result = PodPrebuild::CacheValidator.new(
+        podfile: podfile,
+        prebuilt_lockfile: prebuilt_lockfile
+      ).validate
+      @missed = validation_result.missed
+      @hit = validation_result.hit
+    end
+
+    it "returns changes as missed" do
+      expect(@missed).to eq(Set["A", "D", "E"])
+      expect(@hit).to eq(Set["B", "C"])
+    end
+
+    context "dev pods is disabled" do
+      let(:dev_pods_enabled) { false }
+      it "excludes changes for local pods" do
+        expect(@missed).to eq(Set["A", "D"])
+        expect(@hit).to eq(Set["B", "C"])
+      end
+    end
+  end
+end
