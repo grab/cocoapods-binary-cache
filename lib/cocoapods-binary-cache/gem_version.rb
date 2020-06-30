78d418f3c236c5df5e5e52eb5c067885247deb78
Chris Thuyen <chris.thuyen@grabtaxi.com>
2020-06-30 16:08:51 +0800

    chore(*): extract version to a separate file, to automate bumping version for new releases

--- lib/cocoapods-binary-cache/gem_version.rb
+++
@@ -1,6 +0,0 @@
-# Copyright 2019 Grabtaxi Holdings PTE LTE (GRAB), All rights reserved.
-# Use of this source code is governed by an MIT-style license that can be found in the LICENSE file
-
-module CocoapodsBinaryCache
-  VERSION = "0.1.1".freeze
-end
