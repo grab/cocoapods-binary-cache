74f352b9b316a6b1cafe183902cca596326efe5c
Chris Thuyen <chris.thuyen@grabtaxi.com>
2020-07-24 14:03:42 +0800

    chore(*): remove redundant code

--- lib/cocoapods-binary-cache/pod-binary/helper/feature_switches.rb
+++ lib/cocoapods-binary-cache/pod-binary/helper/feature_switches.rb
@@ -2,40 +2,6 @@ require_relative "../tool/tool"
 require_relative "prebuild_sandbox"
 
 module Pod
-  # a switch for the `pod` DSL to make it only valid for ':binary => true'
-  class Podfile
-    module DSL
-      @@enable_prebuild_patch = false
-
-      # when enable, `pod` function will skip all pods without 'prebuild => true'
-      def self.enable_prebuild_patch(value)
-        @@enable_prebuild_patch = value
-      end
-
-      # --- patch ---
-      old_method = instance_method(:pod)
-
-      define_method(:pod) do |name, *args|
-        if !@@enable_prebuild_patch
-          old_method.bind(self).(name, *args)
-          return
-        end
-
-        # patched content
-        should_prebuild = Pod::Podfile::DSL.prebuild_all
-        local = false
-
-        options = args.last
-        if options.is_a?(Hash) and options[Pod::Prebuild.keyword] != nil
-          should_prebuild = options[Pod::Prebuild.keyword]
-          local = (options[:path] != nil)
-        end
-
-        old_method.bind(self).call(name, *args) if should_prebuild && (!local || Podfile::DSL.dev_pods_enabled)
-      end
-    end
-  end
-
   # a force disable option for integral
   class Installer
     def self.force_disable_integration(value)
