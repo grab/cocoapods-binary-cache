7788b431935077482c97b14b27463ef721c111b7
Chris Thuyen <chris.thuyen@grabtaxi.com>
2020-08-07 16:13:51 +0800

    fix(*): false alarms in the integration diagnosis

--- lib/cocoapods-binary-cache/diagnosis/integration.rb
+++ lib/cocoapods-binary-cache/diagnosis/integration.rb
@@ -3,13 +3,15 @@ require_relative "base"
 module PodPrebuild
   class IntegrationDiagnosis < BaseDiagnosis
     def run
-      integrated = @standard_sandbox.root.glob("*/*.framework").map { |p| p.dirname.basename.to_s }
       should_be_integrated = if Pod::Podfile::DSL.prebuild_job? \
                              then @cache_validation.hit + @cache_validation.missed \
                              else @cache_validation.hit \
                              end
-      should_be_integrated = should_be_integrated.map { |name| name.split("/")[0] }
-      unintegrated = (should_be_integrated - integrated)
+      should_be_integrated = should_be_integrated.map { |name| name.split("/")[0] }.to_set
+      unintegrated = should_be_integrated.reject do |name|
+        framework_path = @standard_sandbox.pod_dir(name) + "#{name}.framework"
+        framework_path.exist?
+      end
       Pod::UI.puts "🚩 Unintegrated frameworks: #{unintegrated}".yellow unless unintegrated.empty?
     end
   end
