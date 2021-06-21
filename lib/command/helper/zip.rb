1b2aa679ba33377dc4cf1e96c499e1d526d5c64a
Thuyen Trinh <trinhngocthuyen@gmail.com>
2021-06-21 20:41:43 +0800

    fix(*): do not overwrite files in unzip

--- lib/command/helper/zip.rb
+++ lib/command/helper/zip.rb
@@ -12,7 +12,7 @@ module PodPrebuild
 
     def self.unzip(path, to_dir: nil)
       cmd = []
-      cmd << "unzip -q" << path
+      cmd << "unzip -nq" << path
       cmd << "-d" << to_dir unless to_dir.nil?
       `#{cmd.join(" ")}`
     end
